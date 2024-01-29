a86f6e08c1ce142de6ab354a37906d899ff2bf9f
TaigaAnko <antfgxgres@gmail.com>
2024-01-29 16:00:06 +0900

    add api.py

---
+++ api.py
@@ -0,0 +1,24 @@
+import requests
+
+class NotionAPI:
+    def __init__(self, id=None, token=None, text_output=None) -> None:
+        url = "https://api.notion.com/v1/pages"
+        headers = {
+        "Accept": "application/json",
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + token,
+        }
+        payload = {
+            "parent": {"database_id": id},
+            "properties": {
+                "名前": {
+                    "title": [{"text": {"content": text_output}}],
+                },
+            },
+        }
+        response = requests.post(url, json=payload, headers=headers) 
+
+        result_dict = response.json()
+        result = result_dict["object"]
+        print(result)
