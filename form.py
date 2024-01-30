38f6cd071868471e3f504df6d2222f16aad2b69a
TaigaAnko <antfgxgres@gmail.com>
2024-01-30 10:15:26 +0900

    fixed all

--- form.py
+++ form.py
@@ -2,13 +2,13 @@ import tkinter as tk
 
 
 class From(tk.Frame):
-    def __init__(self, root=None):
-        super().__init__(width=30, height=20, borderwidth=1, relief="groove")
+    def __init__(self, root=None, checkbox_text=None):
+        super().__init__(root, width=30, height=20, borderwidth=1, relief="groove")
 
         self.root = root
         self.pack()
         self.pack_propagate(0)
-        self.form_widgets()
+        self.form_widgets(checkbox_text)
 
     def form_widgets(self, checkbox_text=None):
         self.var_checkbox = tk.BooleanVar()
@@ -21,10 +21,8 @@ class From(tk.Frame):
         checkbox.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 
         # テキストボックス1
-        text_var1 = tk.StringVar()
-        self.entry = tk.Entry(
-            self, width=40, state=tk.DISABLED, textvariable=text_var1
-        )
+        text_var = tk.StringVar()
+        self.entry = tk.Entry(self, width=40, state=tk.DISABLED, textvariable=text_var)
         self.entry.grid(row=0, column=1, padx=5, pady=5)
 
     def change_editability(self):
