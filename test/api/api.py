cfd5bf5ad8be5b9afe50aa8f8c074c6010b3a373
TaigaSato <taiga.sato@arsaga.jp>
2025-02-17 15:04:25 +0900

    pytestに新たなテストを追加

--- test/api/api.py
+++ test/api/api.py
@@ -3,7 +3,9 @@ import requests
 
 VALIDSETTINGS = "VALIDSETTINGS"
 INVALIDSETTINGS = "INVALIDSETTINGS"
+OLDVALIDSETTINGS = "OLDVALIDSETTINGS"
 TOKEN = "token"
+ID = "id"
 
 
 def test_token_vaild():
@@ -34,5 +36,47 @@ def test_token_invaild():
 
     response = requests.get(url, headers=headers)
 
-    assert response.status_code == 401 or 404
+    assert response.status_code == 400 or 401 or 404
+    assert response.json()["object"] == "error"
+
+
+def test_exist_url():
+    config = configparser.ConfigParser()
+    config.read("config/config.ini")
+
+    url = f"https://api.notion.com/v1/databases/{config[VALIDSETTINGS][ID]}/query"
+    headers = {
+        "Authorization": f"Bearer {config[VALIDSETTINGS][TOKEN]}",
+        "Notion-Version": "2022-06-28",
+    }
+    response = requests.post(url, headers=headers)
+    assert response.status_code == 200
+    assert response.json()["object"] == "list"
+
+
+def test_not_exist_url():
+    config = configparser.ConfigParser()
+    config.read("config/config.ini")
+
+    url = f"https://api.notion.com/v1/databases/{config[INVALIDSETTINGS][ID]}/query"
+    headers = {
+        "Authorization": f"Bearer {config[INVALIDSETTINGS][TOKEN]}",
+        "Notion-Version": "2022-06-28",
+    }
+    response = requests.post(url, headers=headers)
+    assert response.status_code == 400 or 401 or 404
+    assert response.json()["object"] == "error"
+
+
+def test_not_exist_invaild_url():
+    config = configparser.ConfigParser()
+    config.read("config/config.ini")
+
+    url = f"https://api.notion.com/v1/databases/{config[INVALIDSETTINGS][ID]}/query"
+    headers = {
+        "Authorization": f"Bearer {config[INVALIDSETTINGS][TOKEN]}",
+        "Notion-Version": "2022-06-28",
+    }
+    response = requests.post(url, headers=headers)
+    assert response.status_code == 400 or 401 or 404
     assert response.json()["object"] == "error"
